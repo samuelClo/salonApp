9df32442b6d188cdbf778e69cfdae88e29a757d7
Yann Grillon <yanngrillon@MacBook-Pro-de-Yann.local>
2021-03-19 16:58:43 +0100

    refactoring folder, add principal screens, add tailwind

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
-import {createAppContainer} from 'react-navigation';
 import React, { Component } from "react";
+import {createBottomTabNavigator, createAppContainer} from 'react-navigation';
 import {createSharedElementStackNavigator} from 'react-navigation-shared-element';
 import {enableScreens} from 'react-native-screens';
-import Main from './Main';
-import Detail from './Detail';
+import Map from './Screens/Map'
+import QrCode from './Screens/QrCode'
+import Events from './Screens/Events'
 import { Provider } from 'react-redux'
 import createStore from './redux'
 enableScreens();
 
 // Instead of calling `createStackNavigator`, wrap it using `createSharedElementStackNavigator`
-const StackNavigator = createSharedElementStackNavigator(
-  {
-    Main: {screen: Main},
-    Detail: {screen: Detail},
-  },
-  {
-    headerMode: 'none',
-    initialRouteName: 'Main',
-  }
-);
+// const StackNavigator = createSharedElementStackNavigator(
+//   {
+//     Main: {screen: Main},
+//     Detail: {screen: Detail},
+//   },
+//   {
+//     headerMode: 'none',
+//     initialRouteName: 'Main',
+//   }
+// );
 
-const AppContainer = createAppContainer(StackNavigator);
+const TabNavigator = createBottomTabNavigator({
+  Map: Map,
+  QrCode: QrCode,
+  Events: Events
+});
+
+const AppContainer = createAppContainer(TabNavigator);
 const { store } = createStore()
 class RootContainer extends Component{
     render(){
