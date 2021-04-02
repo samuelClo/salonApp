14dfc6240410b1887061cb7c34bf51d0b5d773a2
Ariel <arielthibault@yahoo.fr>
2021-04-02 15:21:08 +0200

    resolve

--- screens/QrCode.js
+++ screens/QrCode.js
@@ -1,64 +1,99 @@
-'use strict';
+import React, { Component, Fragment } from 'react';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import styles from './scanStyle'
+import {TouchableOpacity, Text, StatusBar, Linking, View} from 'react-native';
 
-import React, { Component } from 'react';
+export default class ScanScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            scan: true,
+            ScanResult: false,
+            result: null
+        };
+    }
 
-import {
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  Linking
-} from 'react-native';
+    onSuccess = (e) => {
+        const check = e.data.substring(0, 4);
+        console.log('scanned data' + check);
+        this.setState({
+            result: e,
+            scan: false,
+            ScanResult: true
+        })
+        if (check === 'http') {
+            Linking
+                .openURL(e.data)
+                .catch(err => console.error('An error occured', err));
 
+<<<<<<< Updated upstream
 import QRCodeScanner from 'react-native-qrcode-scanner';
 <<<<<<< HEAD
 import { RNCamera } from 'react-native-camera';
 =======
 >>>>>>> team-nav
+=======
+>>>>>>> Stashed changes
 
-export default class ScanScreen extends Component {
-  onSuccess = e => {
-    Linking.openURL(e.data).catch(err =>
-      console.error('An error occured', err)
-    );
-  };
+        } else {
+            this.setState({
+                result: e,
+                scan: false,
+                ScanResult: true
+            })
+        }
 
-  render() {
-    return (
-      <QRCodeScanner
-        showMarker
-        onRead={this.onSuccess}
-        reactivate
-        topContent={
-          <Text style={styles.centerText}>
-            check your event
-          </Text>
-        }
-        bottomContent={
-          <TouchableOpacity style={styles.buttonTouchable}>
-            <Text style={styles.buttonText}>Scan</Text>
-          </TouchableOpacity>
-        }
-      />
-    );
-  }
+    }
+    scanAgain = () => {
+        this.setState({
+            scan: true,
+            ScanResult: false
+        })
+    }
+    render() {
+        const { scan, ScanResult, result } = this.state
+        return (
+            <View style={styles.scrollViewStyle}>
+                <Fragment>
+                    <StatusBar barStyle="dark-content" />
+                    <Text style={styles.textTitle}>Scan the Stand near to you</Text>
+                    {ScanResult &&
+                        <Fragment>
+                            <Text style={styles.textTitle1}>Result !</Text>
+                            <View style={ScanResult ? styles.scanCardView : styles.cardView}>
+                                <Text>Result : {result.data}</Text>
+                                <TouchableOpacity onPress={this.scanAgain} style={styles.buttonTouchable}>
+                                    <Text style={styles.buttonTextStyle}>Click to Scan again!</Text>
+                                </TouchableOpacity>
+
+                            </View>
+                        </Fragment>
+                    }
+
+
+                    {scan &&
+                        <QRCodeScanner
+                            reactivate={true}
+                            showMarker={true}
+                            ref={(node) => { this.scanner = node }}
+                            onRead={this.onSuccess}
+                            topContent={
+                                <Text style={styles.centerText}>
+                                  you'll find all the information you want thanks to that
+                                </Text>
+                            }
+                            bottomContent={
+                                <View>
+                                    <TouchableOpacity style={styles.buttonTouchable} onPress={() => this.scanner.reactivate()}>
+                                        <Text style={styles.buttonTextStyle}>Scan !</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            }
+                        />
+                    }
+                </Fragment>
+            </View>
+
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777'
-  },
-  textBold: {
-    fontWeight: '500',
-    color: '#000'
-  },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)'
-  },
-  buttonTouchable: {
-    padding: 16
-  }
-});
