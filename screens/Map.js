9eebe54d92da4dda639cc7fb6430aca4f21ed96e
Ariel <arielthibault@yahoo.fr>
2021-04-02 15:40:56 +0200

    ah

--- screens/Map.js
+++ screens/Map.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import SplashScreen from 'react-native-splash-screen'
 import { View } from 'react-native'
-import Header from "../components/Header";
+import Header from "../components/Header.js";
 
 
 export default class Map extends React.Component {
