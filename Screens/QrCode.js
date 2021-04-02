df87ff13fae14bb5ae03d3218a8aeaea4af6e68b
Ariel <arielthibault@yahoo.fr>
2021-04-02 13:05:24 +0200

    add qrCode reader

--- Screens/QrCode.js
+++ Screens/QrCode.js
@@ -1,8 +1,61 @@
-import React from 'react'
-import { View } from 'react-native'
+'use strict';
 
-export default class QrCode extends React.Component {
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Linking
+} from 'react-native';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
+
+export default class ScanScreen extends Component {
+  onSuccess = e => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occured', err)
+    );
+  };
+
   render() {
-    return <View/>
+    return (
+      <QRCodeScanner
+        showMarker
+        onRead={this.onSuccess}
+        reactivate
+        topContent={
+          <Text style={styles.centerText}>
+            check your event
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>Scan</Text>
+          </TouchableOpacity>
+        }
+      />
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777'
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000'
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)'
+  },
+  buttonTouchable: {
+    padding: 16
+  }
+});
